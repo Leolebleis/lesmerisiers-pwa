5a458fb89b9c91bd96718a570cadd79a3f2a4920
leo <lelebleis@deloitte.co.uk>
2020-05-11 20:03:42 +0100

    Added react-spring menu in guide

--- src/components/Guide/Guide.jsx
+++ src/components/Guide/Guide.jsx
@@ -1,11 +1,37 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import Sidebar from "../react-springs/TreeList/Sidebar";
 
 export default class Guide extends React.Component {
+  state = {
+    active: "main",
+  };
+
+  changeContent = (props) => {
+    console.log(this.props.name);
+    console.log("hello");
+  };
+
   render() {
     return (
-      <React.Fragment>
-        <div />
-      </React.Fragment>
+      <Container>
+        <Row>
+          <Col className="text-right col-auto">
+            <Card className="p-4 align-content-end d-inline-flex text-left border-0">
+              <Sidebar changeContent={this.changeContent} />
+            </Card>
+          </Col>
+          <Col className="col-auto">
+            <Card className="p-4 align-content-end d-inline-flex text-left border-0">
+              <p>Text</p>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
